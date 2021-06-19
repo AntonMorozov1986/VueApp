2dbf5806dd8fb4b450729aed5402e7a10604c5f8
Anton <amorozov86@gmail.com>
2021-06-19 19:50:32 +0300

    Создание страниц
    Созданы страницы Hime, Login, Registration, Orders, New Ad.

---
+++ src/components/Ads/NewAd.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Create New Ad</h1>
+        <v-form
+            ref="form"
+            v-model="valid"
+            validation
+        >
+
+          <v-text-field
+              class="mb-3"
+              name="title"
+              label="Ad title"
+              type="text"
+              v-model="title"
+              :rules="[v => !!v || 'The Title is required']"
+          ></v-text-field>
+
+          <v-textarea
+              class="mb-3"
+              name="description"
+              label="Ad description"
+              no-resize
+              rows="8"
+              v-model="description"
+              :rules="[v => !!v || 'The Description is required']"
+          ></v-textarea>
+        </v-form>
+
+        <v-layout>
+          <v-flex>
+            <v-btn
+                block
+                color="primary"
+                class="mb-3 white--text"
+            >
+              Upload image
+              <v-icon
+                  right
+                  dark
+              >
+                mdi-cloud-upload
+              </v-icon>
+            </v-btn>
+
+            <img class="new-ad-image" src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" alt="">
+
+            <v-switch
+                class="mt-0 mb-3 pt-0"
+                v-model="promo"
+                label="Add to promo?"
+            ></v-switch>
+
+            <v-btn
+              :disabled="!valid"
+              class="primary"
+              @click="createAd"
+            >Create Ad</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "NewAd",
+  data () {
+    return {
+      valid: false,
+      title: '',
+      description: '',
+      promo: false,
+
+    }
+  },
+
+  methods: {
+    createAd () {
+      if (this.$refs.form.validate()) {
+        //some logic
+
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        }
+
+        console.log(ad);
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.new-ad-image {
+  display: block;
+  margin: 0 auto 12px auto;
+  width: 100%;
+}
+</style>
---
+++ src/components/User/Orders.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3 class="mt-5">
+        <v-list
+            subheader
+            two-line
+            flat
+        >
+          <h1 class="text--secondary mb-3">Orders</h1>
+
+            <v-list-item
+              v-for="order of orders"
+              :key="order.id"
+            >
+
+              <v-checkbox
+                v-model="order.isDone"
+                :disabled="order.isDone"
+                @change="markDone(order)"
+              ></v-checkbox>
+
+
+              <v-list-item-content>
+                <v-list-item-title>{{ order.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn
+                  class="primary"
+                  :to="'/ad/' + order.adId"
+                >Open</v-btn>
+              </v-list-item-action>
+            </v-list-item>
+
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Orders",
+  data () {
+    return {
+      orders: [
+        {
+          id: 'o123',
+          name: 'John',
+          phone: '8-921-125-25-55',
+          adId: '102',
+          isDone: false
+        }
+      ]
+    }
+  },
+
+  methods: {
+    markDone (order) {
+      alert('done');
+      order.isDone = true;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
